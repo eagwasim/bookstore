2616d55d12c00c91c8b166cfe9c0109fd6de7779
unknown <agwasim.e@gmail.com>
2017-11-02 13:15:09 +0100

    Fixed Build

--- src/main/java/controllers/ApplicationController.java
+++ src/main/java/controllers/ApplicationController.java
@@ -50,7 +50,7 @@ public class ApplicationController {
     public Result helloWorldJson() {
         
         SimplePojo simplePojo = new SimplePojo();
-        simplePojo.content = "Hello World! Hello Json! this is your boy, assertion for a failed test. further messing up the build";
+        simplePojo.content = "Hello World! Hello Json!";
 
         return Results.json().render(simplePojo);
 
